70a94248adea5a4cbb0e03ab3db7740447b639e9
Ansar98858 <kkansarkk64@gmail.com>
2025-06-02 16:17:11 +0530

    Add files via upload

---
+++ Java/Demopen.java
@@ -0,0 +1,11 @@
+class Demopen
+{
+	public static void main(String[] argStrings)
+	{
+		Pen p=new Pen();
+		p.name="elkos";
+		p.color="blue";
+		System.out.println("This pen name is:" + p.name);
+		System.out.println("This pen color is:"+p.color);
+	}
+}
