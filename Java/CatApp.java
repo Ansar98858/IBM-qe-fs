4af4b9d520363b3485f965cc0b7f571e975573a7
Ansar98858 <kkansarkk64@gmail.com>
2025-06-02 16:11:27 +0530

    Add files via upload

---
+++ Java/CatApp.java
@@ -0,0 +1,17 @@
+
+public class CatApp {
+	public static void main(String[] args)
+	
+	
+	{
+		Cat c=new Cat();
+		c.name="Dillu";
+		c.color="Black";
+		c.age=2;
+		System.out.println("This cat name is:" + c.name);
+		System.out.println("This cat color is:"+c.color);
+		System.out.println("This cat age is:" + c.name);
+		
+	}
+
+}
